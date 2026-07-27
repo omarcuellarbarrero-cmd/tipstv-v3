44e6335f7969bf60aa91bb88b1f7d4f00f651b10
Omar Cuellar Barrero <omarcuellarbarrero@gmail.com>
2026-07-27 15:00:53 -0500

    Fix: corrige ruta de import de BackButton

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import BackButton from "./BackButton";
 import { useState } from "react"
 import { signIn } from "next-auth/react"
 import { useRouter } from "next/navigation"
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
-import BackButton from "./BackButton";
+
 
 function App() {
   return (
